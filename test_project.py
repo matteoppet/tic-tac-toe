357684fcb1d80e365efe4f68aa3708d234c028ce
Matteo <matteopet07@gmail.com>
2023-02-24 16:18:47 +0100

    Solved a bug

--- test_project.py
+++ test_project.py
@@ -11,24 +11,26 @@ def test_check_tris():
         X in green = \x1b[32mX\x1b[0m
         O in red = \x1b[31mO\x1b[0m
     '''
+    dict_symbols = {"Player": "X", "Bot": "O"}
+
 
     # Win X
     win_x_horizontal = ['\x1b[32mX\x1b[0m', '\x1b[32mX\x1b[0m', '\x1b[32mX\x1b[0m', 4, 5, 6, 7, 8, 9]
     win_x_diagonal = ['\x1b[32mX\x1b[0m', 2, 3, 4, '\x1b[32mX\x1b[0m', 6, 7, 8, '\x1b[32mX\x1b[0m']
     win_x_vertical = ['\x1b[32mX\x1b[0m', 2, 3, '\x1b[32mX\x1b[0m', 5, 6, '\x1b[32mX\x1b[0m', 8, 9]
 
-    assert check_tris("X", win_x_horizontal) == "\nThe winner of this game is: \x1b[32mX\x1b[0m"
-    assert check_tris("X", win_x_diagonal) == "\nThe winner of this game is: \x1b[32mX\x1b[0m"
-    assert check_tris("X", win_x_vertical) == "\nThe winner of this game is: \x1b[32mX\x1b[0m"
+    assert check_tris("X", win_x_horizontal, dict_symbols) == "\nThe winner of this game is: \x1b[32mX\x1b[0m"
+    assert check_tris("X", win_x_diagonal, dict_symbols) == "\nThe winner of this game is: \x1b[32mX\x1b[0m"
+    assert check_tris("X", win_x_vertical, dict_symbols) == "\nThe winner of this game is: \x1b[32mX\x1b[0m"
 
     # Win O
     win_o_horizontal = ['\x1b[31mO\x1b[0m', '\x1b[31mO\x1b[0m', '\x1b[31mO\x1b[0m', 4, 5, 6, 7, 8, 9]
     win_o_diagonal = ['\x1b[31mO\x1b[0m', 2, 3, 4, '\x1b[31mO\x1b[0m', 6, 7, 8, '\x1b[31mO\x1b[0m']
     win_o_vertical = ['\x1b[31mO\x1b[0m', 2, 3, '\x1b[31mO\x1b[0m', 5, 6, '\x1b[31mO\x1b[0m', 8, 9]
 
-    assert check_tris("O", win_o_horizontal) == "\nThe winner of this game is: \x1b[31mO\x1b[0m"
-    assert check_tris("O", win_o_diagonal) == "\nThe winner of this game is: \x1b[31mO\x1b[0m"
-    assert check_tris("O", win_o_vertical) == "\nThe winner of this game is: \x1b[31mO\x1b[0m"
+    assert check_tris("O", win_o_horizontal, dict_symbols) == "\nThe winner of this game is: \x1b[31mO\x1b[0m"
+    assert check_tris("O", win_o_diagonal, dict_symbols) == "\nThe winner of this game is: \x1b[31mO\x1b[0m"
+    assert check_tris("O", win_o_vertical, dict_symbols) == "\nThe winner of this game is: \x1b[31mO\x1b[0m"
 
 
 def test_first_move_information():
